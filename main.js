fb6aeaf5daee40467be4ac32dac16a01ccdbbd51
unknown <Juan Carlos>
2022-07-04 17:29:47 -0600

    clase1 js

---
+++ main.js
@@ -0,0 +1,84 @@
+var String = ('bootcamp');
+console.log(String);
+
+var numero1 = 5
+var numero2 = 5
+resultado = numero1 + numero2
+console.log(resultado);
+
+var miObjeto = {
+  nombre: "jorge",
+  apellido: "lopez",
+  edad: "30 años"
+}
+console.log(miObjeto);
+
+var edad = 18;
+
+if(edad>18) 
+  alert('eres mayor de edad');
+else if(edad<18)
+  alert('eres menor de edad');
+else
+  alert('tienes 18 años');
+
+
+//true para colocar la palabra hola false para adios
+var aux = true; 
+var asignada = aux?'hola':'adios';
+alert(asignada);
+
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var division = numero1 / numero2;
+var suma = numero1 + numero2;
+
+let verdadero = true;
+let falso = false;
+let v = boolean = (true);
+let f = boolean = (false);
+
+console.log(verdadero, falso, v , f);
+console.log(typeof verdadero, typeof falso);
+
+var String = ('conjuncion');
+console.log(String);
+var Verdadero = true;
+var Falso = false;
+
+var resultado = Verdadero && Verdadero;
+console.log(resultado);
+var resultado = Verdadero && Falso;
+console.log(resultado);
+var resultado = Falso && Verdadero;
+console.log(resultado);
+var resultado = Falso && Falso;
+console.log(resultado);
+
+var String = ('disyuncion');
+console.log(String);
+var Verdadero = true;
+var Falso = false;
+
+var resultado = Verdadero || Verdadero;
+console.log(resultado);
+var resultado = Verdadero || Falso;
+console.log(resultado);
+var resultado = Falso || Verdadero;
+console.log(resultado);
+var resultado = Falso || Falso;
+console.log(resultado);
+
+var String = ('negacion');
+console.log(String);
+var Verdadero = true;
+var Falso = false;
+
+var resultado = Verdadero && !Verdadero;
+console.log(resultado);
+var resultado = Verdadero && !Falso;
+console.log(resultado);
+var resultado = Falso && !Verdadero;
+console.log(resultado);
+var resultado = Falso && !Falso;
+console.log(resultado);
